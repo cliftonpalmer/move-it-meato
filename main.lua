1dd2208addc67fbb59dcac52eeb542d35d5f7822
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-20 05:49:29 -0600

    Changed to use Box2D physics for movement

--- main.lua
+++ main.lua
@@ -1,12 +1,47 @@
-local player = require 'player'
-local block = require 'block'
+-- physics
+local world = require 'world'
+local velocityiterations = 8
+local positioniterations = 3
 
+-- player
+local player = {}
+player.width = 50 -- meters
+player.height = 50
+player.mass = 150 -- kilograms
+player.acceleration = 400
+player.position = {}
+player.position.x = love.graphics.getWidth() / 2
+player.position.y = love.graphics.getHeight() / 2
+
+player.physics = {}
+player.physics.body = love.physics.newBody(world, player.position.x, player.position.y, 'dynamic')
+player.physics.body:setMass(player.mass)
+player.physics.shape = love.physics.newRectangleShape( player.width, player.height )
+player.physics.fixture = love.physics.newFixture( player.physics.body, player.physics.shape, 1.0 )
+player.physics.fixture:setFriction(0.5)
+
+function player.update(dt)
+    if love.keyboard.isDown("up") then
+        player.physics.body:applyForce(0, -player.acceleration)
+    end
+    if love.keyboard.isDown("down") then
+        player.physics.body:applyForce(0, player.acceleration)
+    end
+
+    if love.keyboard.isDown("left") then
+        player.physics.body:applyForce(-player.acceleration, 0)
+    end
+    if love.keyboard.isDown("right") then
+        player.physics.body:applyForce(player.acceleration, 0)
+    end
+end
+
+-- main
 function love.load()
 end
 
 function love.draw()
-    player.draw()
-    block.draw()
+    love.graphics.polygon('fill', player.physics.body:getWorldPoints(player.physics.shape:getPoints()))
 end
 
 function love.update(dt)
@@ -16,7 +51,7 @@ function love.update(dt)
     end
 
     player.update(dt)
-    block.update(dt)
+    world:update( dt, velocityiterations, positioniterations )
 end
 
 function love.quit()
---
+++ world.lua
@@ -0,0 +1,6 @@
+local xg = 0
+local yg = 0
+local sleep = true
+local world = love.physics.newWorld( xg, yg, sleep )
+
+return world

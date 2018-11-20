d760f70baf347304f31157edf90fe334ffec4f12
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-19 21:06:17 -0600

    Moved player to separate file

--- main.lua
+++ main.lua
@@ -1,52 +1,5 @@
--- define player
-local player = {}
+local player = require 'player'
 
-player.width = 50
-player.height = 50
-player.acceleration = 0.2
-player.mass = 100
-player.friction = 0.96
-player.velocity = {
-    x = 0,
-    y = 0,
-}
-player.position = {
-    x = love.graphics.getWidth() / 2,
-    y = love.graphics.getHeight() / 2,
-}
-
-function player.update(dt)
-    vector = { x = 0, y = 0 }
-
-    -- get relative velocity
-    if love.keyboard.isDown("up") then
-        vector.y = vector.y - player.acceleration * dt
-    end
-    if love.keyboard.isDown("down") then
-        vector.y = vector.y + player.acceleration * dt
-    end
-
-    if love.keyboard.isDown("right") then
-        vector.x = vector.x + player.acceleration * dt
-    end
-    if love.keyboard.isDown("left") then
-        vector.x = vector.x - player.acceleration * dt
-    end
-
-    -- update player velocity
-    player.velocity.x = player.velocity.x * player.friction + vector.x * player.mass
-    player.velocity.y = player.velocity.y * player.friction + vector.y * player.mass
-
-    -- update player position
-    player.position.x = player.position.x + player.velocity.x
-    player.position.y = player.position.y + player.velocity.y
-end
-
-function player.draw()
-    love.graphics.rectangle('fill', player.position.x, player.position.y, player.width, player.height)
-end
-
--- main
 function love.load()
 end
 

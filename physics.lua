2dc12072ae496eee857819bafccbfba9f3bb6e55
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-26 06:27:02 -0600

    Changed joint type to distance

--- physics.lua
+++ physics.lua
@@ -5,7 +5,7 @@ function player_grip_fixture(f)
         local body1 = player.fixture:getBody()
         local body2 = f:getBody()
         local x, y = body1:getWorldCenter()
-        player.joint = love.physics.newPrismaticJoint(body1, body2, x, y, x, y, true)
+        player.joint = love.physics.newDistanceJoint(body1, body2, x, y, x + 1, y + 1, true)
         player.joint_created = love.timer.getTime() + 1.0
     end
 end

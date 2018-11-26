3650566fbf2e336af3446e70cd977de7331e4865
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-26 10:03:24 -0600

    Removed unnecessary if statement

--- physics.lua
+++ physics.lua
@@ -1,13 +1,12 @@
 -- physics for gripping and releasing cargo
 
 function player_grip_fixture(f)
-    if not player.joint then
-        local body1 = player.fixture:getBody()
-        local body2 = f:getBody()
-        local x, y = body1:getWorldCenter()
-        player.joint = love.physics.newDistanceJoint(body1, body2, x, y, x + 1, y + 1, true)
-        player.joint_created = love.timer.getTime() + 1.0
-    end
+    local body1 = player.fixture:getBody()
+    local body2 = f:getBody()
+
+    local x, y = body1:getWorldCenter()
+    player.joint = love.physics.newDistanceJoint(body1, body2, x, y, x + 2, y + 2, true)
+    player.joint_created = love.timer.getTime() + 1.0
 end
 love.handlers.player_grip_fixture = player_grip_fixture
 

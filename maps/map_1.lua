197143fbef0b41ceecaf35b489e57330346dde96
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-26 10:19:22 -0600

    Removed unnecessary cargo class

--- maps/map_1.lua
+++ maps/map_1.lua
@@ -3,7 +3,6 @@
 require 'block'
 require 'player'
 require 'wall'
-require 'cargo'
 
 local width = love.graphics.getWidth()
 local height = love.graphics.getHeight()
@@ -22,7 +21,7 @@ table.insert(blocks, Wall:new(width/2, 0,  width-30, 20))
 table.insert(blocks, Wall:new(width/2, height,  width-30, 20))
 
 -- obstacles
-table.insert(blocks, Cargo:new({width=25, height=25, position = {x=40, y=40}}))
+table.insert(blocks, Block:new({name='cargo', color={0,255,0}, width=25, height=25, position = {x=40, y=40}}))
 table.insert(blocks, Block:new({width=50, height=25, position = {x=200, y=200}}))
 
 map.blocks = blocks

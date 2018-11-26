a4616766e08033879db97aa12a69ba100ab9e5cc
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-26 06:20:10 -0600

    Added bounding box

--- maps/map_1.lua
+++ maps/map_1.lua
@@ -2,12 +2,25 @@
 
 require 'block'
 require 'player'
+require 'wall'
+
+local width = love.graphics.getWidth()
+local height = love.graphics.getHeight()
 
 local map = {}
-map.player = Player:new(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+map.player = Player:new(width/2, height/2)
 
 local blocks = {}
 table.insert(blocks, map.player)
+
+-- bounding box
+table.insert(blocks, Wall:new(0, height/2, 20, height-30))
+table.insert(blocks, Wall:new(width, height/2, 20, height-30))
+
+table.insert(blocks, Wall:new(width/2, 0,  width-30, 20))
+table.insert(blocks, Wall:new(width/2, height,  width-30, 20))
+
+
 table.insert(blocks, Block:new({width=50, height=25, position = {x=200, y=200}}))
 
 map.blocks = blocks

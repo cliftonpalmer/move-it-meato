262015a7696b5ed5215d615dd5f2cf3ba60e9aad
Clifton James Palmer <clifton.palmer@gmail.com>
2018-11-26 11:02:09 -0600

    Made map more exciting

--- maps/map_1.lua
+++ maps/map_1.lua
@@ -8,21 +8,32 @@ local width = love.graphics.getWidth()
 local height = love.graphics.getHeight()
 
 local map = {}
-map.player = Player:new({position={x=width/2, y=height/2}})
+map.player = Player:new({position={x=width-40, y=40}})
 
 local blocks = {}
 table.insert(blocks, map.player)
 
 -- bounding box
-table.insert(blocks, Wall:new(0, height/2, 20, height-30))
-table.insert(blocks, Wall:new(width, height/2, 20, height-30))
+table.insert(blocks, Wall:new({position={x=0, y=height/2}, width=20, height=height-30}))
+table.insert(blocks, Wall:new({position={x=width, y=height/2}, width=20, height=height-30}))
 
-table.insert(blocks, Wall:new(width/2, 0,  width-30, 20))
-table.insert(blocks, Wall:new(width/2, height,  width-30, 20))
+table.insert(blocks, Wall:new({position={x=width/2, y=0}, width=width-30, height=20}))
+table.insert(blocks, Wall:new({position={x=width/2, y=height}, width=width-30, height=20}))
+
+-- other walls
+table.insert(blocks, Wall:new({position={x=width/4 * 3, y=height/4}, width=20, height=height-30}))
+table.insert(blocks, Wall:new({position={x=width/4 * 1, y=height/4 * 3}, width=20, height=height-30}))
 
 -- obstacles
-table.insert(blocks, Block:new({name='cargo', color={0,255,0}, width=25, height=25, position = {x=40, y=40}}))
-table.insert(blocks, Block:new({width=50, height=25, position = {x=200, y=200}}))
+table.insert(blocks, Block:new({
+    name='cargo', color={0,255,0}, mass=30, width=35, height=35, position = {x=40, y=height-40}
+    }))
+
+for i=1,5 do
+    for j=1,5 do
+        table.insert(blocks, Block:new({width=50, height=25, position = {x=100*i, y=100*j}}))
+    end
+end
 
 map.blocks = blocks
 return map

3437c74fed58ed99d06bd0ccab35eaf105e39b96
Clifton James Palmer <clifton.palmer@gmail.com>
2019-09-07 18:51:50 -0500

    Added grab and release

--- player_grab.gd
+++ player_grab.gd
@@ -5,13 +5,15 @@ extends Area2D
 # var b = "text"
 var grabbed = null
 var grabbable = null
+var grab_parent = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func reparent(body):
-	body.get_parent().remove_child(body)
+	grab_parent = body.get_parent()
+	grab_parent.remove_child(body)
 	$grab.add_child(body)
 	
 func grab_body(body):
@@ -19,12 +21,22 @@ func grab_body(body):
 	grabbed = body
 	call_deferred("reparent", body)
 	print("Grabbed " + (body as RigidBody2D).name)
+	
+func release_body(body):
+	(body as RigidBody2D).mode = RigidBody2D.MODE_RIGID
+	grabbed = null
+	$grab.remove_child(body)
+	grab_parent.add_child(body)
+	print("Released " + (body as RigidBody2D).name)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept"):
 		if grabbable and not grabbed:
 			grab_body(grabbable)
+		elif grabbed:
+			release_body(grabbed)
 
 func _on_player_grab_body_entered(body):
 	if body is RigidBody2D:
